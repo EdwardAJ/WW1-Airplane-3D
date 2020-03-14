6b0e087104a0c1384d012dd1a4dafeb3eff34fbb
Akhmal <13517127@std.stei.itb.ac.id>
2020-03-14 20:52:54 +0700

    add wingsbrace

--- src/parts/wingsmain.cpp
+++ src/parts/wingsmain.cpp
@@ -6,9 +6,7 @@
 
 #include "wingsmain.h"
 
-void drawWingsmain() {
-
-    /*Sayap pertama*/
+void drawSideWings() {
     /*Sayap pertama*/
     glBegin(GL_QUADS);
     /* Patokan dari kotak warna pink */
@@ -59,7 +57,9 @@ void drawWingsmain() {
     glVertex3f(1.00, -0.1, -2.0);
 
     glEnd();
+}
 
+void drawUpperWings() {
     /* Sayap kedua */
     glBegin(GL_QUADS);
     /* Patokan dari kotak warna pink */
@@ -110,6 +110,62 @@ void drawWingsmain() {
     glVertex3f(1.00, 0.9, -2.0);
 
     glEnd();
+}
 
+void drawUpperWingBrace() {
+    glBegin(GL_QUADS);
+    /* Patokan dari kotak warna pink */
+    /* x : depan +, belakang - */
+    /* y : atas +, bawah - */
+    /* z : kiri +, kanan - */
 
+    /* Kotak hijau (atas) */
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f( 1.0, 1.0, -0.1);
+    glVertex3f(0.0, 1.0, -0.1);
+    glVertex3f(0.0, 1.0,  0.1);
+    glVertex3f(1.0, 1.0, 0.1);
+
+    /* Kotak oren (bawah) */
+    glColor3f(1.0, 0.1, 0.0);
+    glVertex3f(1.0, -0.75,  0.1);
+    glVertex3f(0.0, -0.75,  0.1);
+    glVertex3f(0.0, -0.75, -0.1);
+    glVertex3f(1.0, -0.75, -0.1);
+
+    /* Kotak merah (kiri) */
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(1.0,  1.0, 0.1);
+    glVertex3f(0.0,  1.0, 0.1);
+    glVertex3f(0.0, -0.75, 0.1);
+    glVertex3f(1.0, -0.75, 0.1);
+
+    /* Kotak kuning (kanan) */
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex3f(1.0, -0.75, -0.1);
+    glVertex3f(0.0, -0.75, -0.1);
+    glVertex3f(0.0,  1.0, -0.1);
+    glVertex3f(1.0,  1.0, -0.1);
+
+    /* Kotak biru (belakang) */
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0,  1.0,  0.1);
+    glVertex3f(0.0,  1.0, -0.1);
+    glVertex3f(0.0, -0.75, -0.1);
+    glVertex3f(0.0, -0.75,  0.1);
+
+    /* Kotak pink (depan) */
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(1.0,  1.0, -0.1);
+    glVertex3f(1.0,  1.0,  0.1);
+    glVertex3f(1.0, -0.75,  0.1);
+    glVertex3f(1.0, -0.75, -0.1);
+
+    glEnd();
+}
+
+void drawWingsmain() {
+    drawSideWings();
+    drawUpperWings();
+    drawUpperWingBrace();
 }
