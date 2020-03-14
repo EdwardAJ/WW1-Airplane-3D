67d847ab56bd72d61626c9b21b5d9351a7067384
Akhmal <13517127@std.stei.itb.ac.id>
2020-03-14 17:54:34 +0700

    airplane body fixed

--- src/main.cpp
+++ src/main.cpp
@@ -93,47 +93,51 @@ void draw3D () {
 
     glBegin(GL_QUADS);
     /* Patokan dari kotak warna pink */
+    /* x : depan +, belakang - */
+    /* y : atas +, bawah - */
+    /* z : kiri +, kanan - */
+
     /* Kotak hijau (atas) */
     glColor3f(0.0, 1.0, 0.0);
-    glVertex3f( 2.0, 1.0, -1.0);
-    glVertex3f(-2.0, 1.0, -1.0);
-    glVertex3f(-2.0, 1.0,  1.0);
-    glVertex3f( 2.0, 1.0, 1.0);
+    glVertex3f( 2.0, 0.75, -0.5);
+    glVertex3f(-2.0, 0.75, -0.5);
+    glVertex3f(-2.0, 0.75,  0.5);
+    glVertex3f( 2.0, 0.75, 0.5);
 
     /* Kotak oren (bawah) */
     glColor3f(1.0, 0.5, 0.0);
-    glVertex3f( 2.0, -1.0,  1.0);
-    glVertex3f(-2.0, -1.0,  1.0);
-    glVertex3f(-2.0, -1.0, -1.0);
-    glVertex3f( 2.0, -1.0, -1.0);
+    glVertex3f( 2.0, -0.75,  0.5);
+    glVertex3f(-2.0, -0.75,  0.5);
+    glVertex3f(-2.0, -0.75, -0.5);
+    glVertex3f( 2.0, -0.75, -0.5);
 
     /* Kotak merah (kiri) */
     glColor3f(1.0, 0.0, 0.0);
-    glVertex3f( 2.0,  1.0, 1.0);
-    glVertex3f(-2.0,  1.0, 1.0);
-    glVertex3f(-2.0, -1.0, 1.0);
-    glVertex3f( 2.0, -1.0, 1.0);
+    glVertex3f( 2.0,  0.75, 0.5);
+    glVertex3f(-2.0,  0.75, 0.5);
+    glVertex3f(-2.0, -0.75, 0.5);
+    glVertex3f( 2.0, -0.75, 0.5);
 
     /* Kotak kuning (kanan) */
     glColor3f(1.0, 1.0, 0.0);
-    glVertex3f( 2.0, -1.0, -1.0);
-    glVertex3f(-2.0, -1.0, -1.0);
-    glVertex3f(-2.0,  1.0, -1.0);
-    glVertex3f( 2.0,  1.0, -1.0);
+    glVertex3f( 2.0, -0.75, -0.5);
+    glVertex3f(-2.0, -0.75, -0.5);
+    glVertex3f(-2.0,  0.75, -0.5);
+    glVertex3f( 2.0,  0.75, -0.5);
 
     /* Kotak biru (belakang) */
     glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(-2.0,  1.0,  1.0);
-    glVertex3f(-2.0,  1.0, -1.0);
-    glVertex3f(-2.0, -1.0, -1.0);
-    glVertex3f(-2.0, -1.0,  1.0);
+    glVertex3f(-2.0,  0.75,  0.5);
+    glVertex3f(-2.0,  0.75, -0.5);
+    glVertex3f(-2.0, -0.75, -0.5);
+    glVertex3f(-2.0, -0.75,  0.5);
 
     /* Kotak pink (depan) */
     glColor3f(1.0, 0.0, 1.0);
-    glVertex3f(2.0,  1.0, -1.0);
-    glVertex3f(2.0,  1.0,  1.0);
-    glVertex3f(2.0, -1.0,  1.0);
-    glVertex3f(2.0, -1.0, -1.0);
+    glVertex3f(2.0,  0.75, -0.5);
+    glVertex3f(2.0,  0.75,  0.5);
+    glVertex3f(2.0, -0.75,  0.5);
+    glVertex3f(2.0, -0.75, -0.5);
 
     glEnd();
 }
